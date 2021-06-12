7ccb896f48a67592bb956620f4564e1584baed74
GuRuGu <peter.k.nets@gmail.com>
2021-06-12 11:14:58 +0300

    Add mongoose

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const express = require("express");
 const cors = require("cors");
 const dns = require("dns");
 const url = require("url");
+const mongoose = require("mongoose");
 
 const app = express();
 
