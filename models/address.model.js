2ec468e005948bdc36cc4ad5633d98f2d1d93a14
GuRuGu <peter.k.nets@gmail.com>
2021-06-12 13:03:23 +0300

    Create Address model

---
+++ models/address.model.js
@@ -0,0 +1,16 @@
+const mongoose = require(mongoose);
+
+const addressSchema = new mongoose.Schema({
+  originalUrl: {
+    type: String,
+    unique: true,
+  },
+  shortUrl: {
+    type: Number,
+    unique: true,
+  },
+});
+
+const Address = mongoose.model("Address", addressSchema);
+
+export default Address;
